5e6d8db9f0b69e28811e45a5fbd9525fe643346d
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-20 14:27:58 -0800

    Add keyboard functionality

--- noise.js
+++ noise.js
@@ -1,3 +1,32 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready(() => {
+  let [cPlay, dPlay, ePlay, fPlay, gPlay, aPlay, bPlay] = [true, true, true, true, true, true, true];
+  const notes = 'abcdefg';
+
+  const play = function play(note) {
+    $(`#${note}Audio`).trigger('load');
+    $(`#${note}Audio`).trigger('play');
+  };
+
+  $('.instrument').on('click', 'button', function () {
+    const note = $(this)[0].innerHTML;
+    console.log(note);
+    play(note);
+  });
+
+  $('body').on('keydown', (event) => {
+    const note = event.key;
+    if (notes.includes(note) && eval(`${note}Play`)) {
+      $(`.${note}`).addClass('active');
+      play(note);
+      eval(`${note}Play = false`);
+    }
+  });
+
+  $('body').on('keyup', (event) => {
+    const note = event.key;
+    $(`.${note}`).removeClass('active');
+    if (notes.includes(note)) {
+      eval(`${note}Play = true`);
+    }
+  });
 });
